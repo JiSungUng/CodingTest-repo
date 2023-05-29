2400a81a90db324ca49804ecd52c177d49b16f3b
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-05-29 11:32:43 +0900

    백준 14681번

--- CodingTest/N_14681.java
+++ CodingTest/N_14681.java
@@ -9,16 +9,16 @@ public class N_14681 {
         int x =sc.nextInt();
         int y= sc.nextInt();
 
-        if(x>0&&y>0){
+        if(x>0&&y>0){// x가 0보다 크고 y가 0보다 클 경우.
             System.out.println("1");
         }
-        if(x<0&&y>0){
+        if(x<0&&y>0){ // x가 0보다 작고 y가 0보다 클 경우
             System.out.println("2");
         }
-        if(x<0&&y<0){
+        if(x<0&&y<0){ //x 가 0보다 작고 y는 0보다 작을 경우
             System.out.println("3");
         }
-        if(x>0&&y<0){
+        if(x>0&&y<0){ // x가 0보다 크고 y는 0 보다 작을 경우
             System.out.println("4");
         }
     }
