da64b3d04a6de34a2a05cd87842e9b2e9091c3ad
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-06-03 22:04:24 +0900

    백준 10250번

--- CodingTest/N_10250.java
+++ CodingTest/N_10250.java
@@ -1,8 +1,29 @@
 package CodingTest;
 //ACM호텔문제
-public class N_10250 {
+//1.엘레베이터로부터 가까운 거리부터 배정
+//2.거리가 같은 경우 낮은 층수부터 배정
+import java.util.Scanner;
+
+public class  N_10250{
     public static void main(String[] args) {
 
-    }
+        Scanner in = new Scanner(System.in);
 
+        int T = in.nextInt();	// 테스트 케이스
+
+
+        for(int i = 0; i < T; i++) {
+
+            int H = in.nextInt();
+            int W= in.nextInt();
+            int N = in.nextInt();
+
+            if(N % H == 0) {
+                System.out.println((H * 100) + (N / H));
+
+            } else {
+                System.out.println(((N % H) * 100) + ((N / H) + 1));
+            }
+        }
+    }
 }
