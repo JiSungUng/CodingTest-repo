22330c85ef1e45017ad66be24fced32c130f644e
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-07-05 20:57:04 +0900

    백준 10773번

--- CodingTest/N_10773.java
+++ CodingTest/N_10773.java
@@ -1,8 +1,32 @@
 package CodingTest;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 //제로 문제
+//0일때 가장 최근의 수를 지운다.
 public class N_10773 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int sum=0;
+        int k=sc.nextInt();
+        Stack<Integer> stack= new Stack<>();
+
+        for(int i=0;i<k;i++){
+
+           int n= sc.nextInt();
+           if(n==0){
+               stack.pop();
+           }else{
+               stack.push(n);
+           }
+
+        }
+        for(int o:stack){
+            sum+=o;
+        }
+        System.out.println(sum);
     }
 }
