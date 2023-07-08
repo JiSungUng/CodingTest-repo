939b186699f214a5dc35aaa6f11bf81b6a9cbbff
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-07-09 02:18:00 +0900

    백준 1436번

---
+++ CodingTest/N_1654.java
@@ -0,0 +1,17 @@
+package CodingTest;
+
+import java.util.Scanner;
+
+//랜선자르기
+public class N_1654 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();//랜선의 개수
+        int cnt= sc.nextInt(); // 총 랜선의 개수
+
+        for(int i=0; i<; i++){
+
+        }
+    }
+}
