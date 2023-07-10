bcc2208cac4f045e0f24b961f71975e06528512b
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-07-11 00:16:26 +0900

    백준 1920번

---
+++ CodingTest/N_1920.java
@@ -0,0 +1,13 @@
+package CodingTest;
+
+import java.util.Scanner;
+
+//수찾기
+public class N_1920 {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int num=sc.nextInt(); //찾을 수
+
+    }
+}
