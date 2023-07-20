1c02eedc5c155679591cee858cf19ff85ea89c70
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-07-20 14:28:11 +0900

    test

--- CodingTest/test.java
+++ CodingTest/test.java
@@ -1,6 +1,7 @@
 package CodingTest;
 
 public class test {
-    public static void main(String args[]){ System.out.println("" + 1 + 2); System.out.println(1 + 2 + ""); }
+    public static void main(String args[]){
+        System.out.println("" + 1 + 2); System.out.println(1 + 2 + ""); }
 
 }
