3ef6d9a668727f7407ff1e666ae2f75922ecea27
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-08-12 13:49:31 +0900

    백준 N_5597

--- CodingTest/N_5597.java
+++ CodingTest/N_5597.java
@@ -1,7 +1,22 @@
 package CodingTest;
 
+import java.util.Scanner;
+
+//과제안 내신 분
 public class N_5597 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] student = new int[31];
+
+        for(int i=1; i<29; i++) {
+            int success = sc.nextInt();
+            student[success] = 1;
+        }
+        for(int i=1; i<student.length; i++) {
+            if(student[i]!=1)
+                System.out.println(i);
+        }
 
     }
 }
