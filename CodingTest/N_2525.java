833c7f5be3d4eafe4695cea61c33f4727cdf704c
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-08-21 14:41:00 +0900

    백준 N_2525

---
+++ CodingTest/N_2525.java
@@ -0,0 +1,24 @@
+package CodingTest;
+
+import java.util.Scanner;
+
+//오븐시계
+public class N_2525 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        int C = in.nextInt();
+
+        int min = 60 * A + B;   // 시 -> 분
+        min += C;
+
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+
+        System.out.println(hour + " " + minute);
+
+    }
+}
