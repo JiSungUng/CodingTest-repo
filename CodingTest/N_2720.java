bd71020861e44662cf05876bc3564e1e8e0658bc
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-01-04 16:31:53 +0900

    백준 N_2720

--- CodingTest/N_2720.java
+++ CodingTest/N_2720.java
@@ -1,7 +1,35 @@
 package CodingTest;
+
+import java.util.Scanner;
+
 //세탁소 사장 동혁 문제
 public class N_2720 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int Q =25;  // Quarter
+        int D = 10; // Dime
+        int N = 5;  // Nickel
+        int P = 1;  // Penny
+        int result[][] = new int[T][4];
+        for (int i = 0; i < T; i++) {
+            int C = sc.nextInt();
+            result[i][0]= C/Q;
+            result[i][1]=C%Q/D;
+            result[i][2]=C%Q%D/N;
+            result[i][3]=C%Q%D%N/P;
+
+        }
+
+        //System.out.println(result[2][3]);
+
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(result[i][j]+" ");
+            }
+            System.out.println();
+        }
+
 
     }
 }
