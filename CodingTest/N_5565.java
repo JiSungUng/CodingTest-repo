8ee6be414ba18d7e1d91edc5f78f812ecb191d3d
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-01-08 16:23:18 +0900

    백준 N_5565

--- CodingTest/N_5565.java
+++ CodingTest/N_5565.java
@@ -6,5 +6,13 @@ import java.util.Scanner;
 public class N_5565 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int total = sc.nextInt();
+        int sum=0;
+        for(int i=0;i<9;i++){
+            int num= sc.nextInt();
+            sum+=num;
+        }
+        System.out.println(total-sum);
     }
 }
