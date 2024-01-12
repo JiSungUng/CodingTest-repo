4265327dbf7a23e1ea95b919c621ed7ada0d24d3
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-01-12 16:00:52 +0900

    백준 N_2581

--- CodingTest/N_2581.java
+++ CodingTest/N_2581.java
@@ -32,7 +32,8 @@ public class N_2581 {
         if (sum == 0) {
             System.out.println(-1);
         } else {
-            System.out.println(sum + "\n" + min);
+            System.out.println(sum);
+            System.out.println(min);
         }
     }
 }
