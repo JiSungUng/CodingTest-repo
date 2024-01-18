d8c16743f1c651d14f50f7157f4a192ca477ce4c
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-01-18 16:49:05 +0900

    백준 N_3046

---
+++ CodingTest/N_3046.java
@@ -0,0 +1,15 @@
+package CodingTest;
+
+import java.util.Scanner;
+
+public class N_3046 {
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int R1 = sc.nextInt(); //
+    int S = sc.nextInt();  // 평균
+
+    System.out.println(S*2-R1);
+
+    }
+}
