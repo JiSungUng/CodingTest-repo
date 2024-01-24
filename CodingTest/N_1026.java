ad948f134e6e1f8e36be494fd742094f6460e802
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-01-24 21:34:52 +0900

    백준 N_1026

--- CodingTest/N_1026.java
+++ CodingTest/N_1026.java
@@ -1,7 +1,18 @@
 package CodingTest;
+
+import java.util.Scanner;
+
 //보물
 public class N_1026 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=0; i<N;i++){
+            int AN= sc.nextInt();
 
+        }
+        for(int i=0; i<N;i++){
+            int BN= sc.nextInt();
+        }
     }
 }
