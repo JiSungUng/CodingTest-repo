0f0ba7ec126b8f9726cc33a44eae52fc4f0c52bd
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-01-25 20:17:30 +0900

    백준 N_1244

--- CodingTest/N_1026.java
+++ CodingTest/N_1026.java
@@ -1,5 +1,5 @@
 package CodingTest;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 //보물
@@ -7,12 +7,22 @@ public class N_1026 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        int sum = 0;
+        int AN[]= new int [N];
+        int BN[]= new int [N];
+
         for(int i=0; i<N;i++){
-            int AN= sc.nextInt();
+            AN [i]= sc.nextInt();
 
         }
+        Arrays.sort(AN);
         for(int i=0; i<N;i++){
-            int BN= sc.nextInt();
+            BN[i]= sc.nextInt();
         }
+        Arrays.sort(BN);
+        for(int i=0; i <N; i++){
+            sum+=AN[i]*BN[N-1-i];
+        }
+        System.out.println(sum);
     }
 }
