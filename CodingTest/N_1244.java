c6cb71fdf9363dbe960958bc7581be1fbe77e937
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-01-26 18:28:48 +0900

    백준 N_1244

--- CodingTest/N_1244.java
+++ CodingTest/N_1244.java
@@ -4,8 +4,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
-import java.util.Scanner;
-
 // 스위치 문제
 public class N_1244 {
     public static void main(String[] args) throws Exception {
